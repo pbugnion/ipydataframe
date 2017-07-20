fcf983c9e432587ed4406898666bef7c48f98127
Pascal Bugnion <pascal@asidatascience.com>
2017-07-20 13:14:46 +0000

    Rename example.js -> DFTransformer

--- js/src/index.js
+++ js/src/index.js
@@ -17,5 +17,5 @@ require('./ipydataframe.css');
 
 
 // Export widget models and views, and the npm package version number.
-module.exports = require('./example.js');
+module.exports = require('./DFTransformer.js');
 module.exports['version'] = require('../package.json').version;
