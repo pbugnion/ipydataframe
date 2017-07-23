bdc1118233819deb0796fd3144e596467d7b7658
Pascal Bugnion <pascal@asidatascience.com>
2017-07-23 12:06:00 +0000

    Add correct JS loaders for tests

--- js/test/webpack.conf.js
+++ js/test/webpack.conf.js
@@ -1,18 +1,19 @@
 var path = require('path');
 
+var loaders = [
+    { test: /\.json$/, loader: 'json-loader' },
+    { test: /\.js$/, loader: 'babel-loader', query: {presets: ['es2015', 'stage-0']}, exclude: /node_modules/ },
+    { test: /\.css$/, loader: "style-loader!css-loader" },
+    { test: /\.(jpg|png|gif)$/, loader: "file-loader" }
+];
+
 module.exports = {
-    entry: './test/src/index.js',
+    entry: ['babel-polyfill', './test/src/index.js'],
     output: {
         path: __dirname + "/build",
         filename: "bundle.js",
-        publicPath: "./build/"
+        publicPath: "./build/",
     },
     bail: true,
-    module: {
-        loaders: [
-            { test: /\.js$/, loader: 'babel-loader', query: {presets: ['es2015', 'stage-0']}, exclude: /node_modules/ },
-            { test: /\.css$/, loader: 'style-loader!css-loader' },
-            { test: /\.json$/, loader: 'json-loader' },
-        ],
-    }
+    module: { loaders: loaders }
 }
